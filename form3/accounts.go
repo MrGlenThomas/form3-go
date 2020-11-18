d144686e1f1a5b3ed31f0278975e3066fe4f3a15
Glen Thomas <glen.thomas@moonpig.com>
2020-11-18 16:18:31 +0000

    Add Create account function

--- form3/accounts.go
+++ form3/accounts.go
@@ -1,5 +1,7 @@
 package form3
 
+import "context"
+
 // AccountsService handles communication with the accounts related
 // methods of the Form3 API.
 //
@@ -57,4 +59,32 @@ type AccountCreation struct {
 type AccountCreationResponse struct {
 	Data  *Account `json:"data"`
 	Links *Links   `json:"links"`
+}
+
+// Register an existing bank account with Form3 or create a new one.
+// The country attribute must be specified as a minimum. Depending on the country,
+// other attributes such as bank_id and bic are mandatory.
+// Form3 generates account numbers and IBANs, where appropriate, in the following cases:
+// - If no account number or IBAN is provided, Form3 generates a valid account number (see below). If supported by the country, an IBAN is also generated.
+// - If an account number is provided but the IBAN is empty, Form3 generates an IBAN if supported by the country.
+// - If only an IBAN is provided, the account number will be left empty.
+// Note that a given bank_id and bic need to be registered with Form3 and connected to your organisation ID.
+// Form3 API docs: https://api-docs.form3.tech/api.html#organisation-accounts-create
+func (s *AccountsService) Create(ctx context.Context, account *Account) (*Account, *Response, error) {
+	u := "organisation/accounts"
+	payload := &AccountCreation{Data: account}
+	req, err := s.client.NewRequest("POST", u, payload)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	req.Header.Set("Accept", jsonApiMediaType)
+
+	m := &AccountCreationResponse{}
+	resp, err := s.client.Do(ctx, req, m)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return m.Data, resp, nil
 }
--- form3/accounts_test.go
+++ form3/accounts_test.go
@@ -1,1 +1,52 @@
 package form3
+
+import (
+	"context"
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+func TestUnit_AccountsService_Create(t *testing.T) {
+	client, mux, _, teardown := setupClientWithStubbedApi()
+	defer teardown()
+
+	mux.HandleFunc("/organisation/accounts", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "POST")
+		testHeader(t, r, "Accept", jsonApiMediaType)
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`
+		{
+			"data": {
+				"id": "d97a4470-299f-11eb-adc1-0242ac120002",
+				"type": "accounts",
+				"organisation_id": "eb0bd6f5-c3f5-44b2-b677-acd23cdde73c",
+				"version": 7,
+				"attributes": {
+					"country": "GB",
+					"base_currency": "GBP"
+				}
+			}
+		}`))
+	})
+
+	createdAccount, _, err := client.Accounts.Create(context.Background(), &Account{})
+	if err != nil {
+		t.Errorf("Create returned error: %v", err)
+	}
+
+	want := &Account{
+		ID:             String("d97a4470-299f-11eb-adc1-0242ac120002"),
+		Type:           String("accounts"),
+		OrganisationId: String("eb0bd6f5-c3f5-44b2-b677-acd23cdde73c"),
+		Version:        Int(7),
+		Attributes: &AccountAttributes{
+			Country:      String("GB"),
+			BaseCurrency: String("GBP"),
+		},
+	}
+	if !reflect.DeepEqual(createdAccount, want) {
+		t.Errorf("Create = %+v, want %+v", createdAccount, want)
+	}
+}
